3b9bf2e9397409467e5ebd59da36583e2be2ff2d
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-09-09 01:29:36 -0500

    done

--- orders.js
+++ orders.js
@@ -31,6 +31,20 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor (items, orderTime, customerId, status) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = "queued";
+    }
+
+    updateStatus(newStatus) {
+        this.status = newStatus;
+
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`);
+    }
+}
 
 
 /*
@@ -45,6 +59,7 @@
 */
 
 //CODE HERE
+let firstTicket = new Ticket(["pizza", "bread", "soda"], "7:03 PM", 575);
 
 
 /*
@@ -54,3 +69,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus("cooking");
